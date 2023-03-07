8849de87017323b74b56c75cc9bb8fbfc4a2d920
nyirurugo omar <nyirurugoomar@gmail.com>
2023-03-07 17:39:59 +0200

    changing color of border buttom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,19 +41,19 @@ useEffect(()=>{
           <div>
             <ul className='hidden md:flex mr-8 '>
                    <NavLink  to='/' >
-                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-[#F1C40F]  text-white p-4 font-bold '>Home</li>
+                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-white  text-white p-4 font-bold '>Home</li>
                     </NavLink>
                 {/* <NavLink to="/aboutus" > */}
-                    <li className='ml-10 text-sm uppercase hover:border-b-2  border-[#F1C40F] text-white p-4 font-bold'>About Us</li>
+                    <li className='ml-10 text-sm uppercase hover:border-b-2  border-white text-white p-4 font-bold'>About Us</li>
                 {/* </NavLink> */}
                 {/* <NavLink to="/services" > */}
-                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-[#F1C40F] text-white p-4 font-bold'>Typologies</li>
+                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-white text-white p-4 font-bold'>Typologies</li>
                     {/* </NavLink> */}
                     {/* <NavLink to="/productpage" > */}
-                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-[#F1C40F] text-white p-4 font-bold'>Financing</li>
+                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-white text-white p-4 font-bold'>Financing</li>
                     {/* </NavLink> */}
                     {/* <NavLink to="/productpage" > */}
-                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-[#F1C40F] text-white p-4 font-bold'>Gallery</li>
+                    <li className='ml-10 text-sm uppercase hover:border-b-2 border-white text-white p-4 font-bold'>Gallery</li>
                     {/* </NavLink> */}
                     {/* <NavLink to="/contact" > */}
                     <li className='ml-40 text-sm uppercase hover:border-b-2 cursor-pointer text-[#0B0961] text-center font-bold bg-white p-4 rounded h-12 w-20  '>Login</li>
